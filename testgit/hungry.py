19d838291891b4b2be4c13ce038c077ff664cec5
avnk89 <avnk89@gmail.com>
2019-09-20 16:42:18 +0530

    fries

--- testgit/hungry.py
+++ testgit/hungry.py
@@ -2,6 +2,7 @@ hungry=input("are you hungry?")
 if hungry=="yes":
     print("eat samosa")
     print("eat green salad")
+    print("play fries")
 else:
     print("do your homework")
     print("play cricket")
