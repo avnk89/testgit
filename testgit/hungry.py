899e149b10a03404bec329e326056673bc408968
avnk89 <avnk89@gmail.com>
2019-09-20 15:57:44 +0530

    new commit for better learning after paradise

--- testgit/hungry.py
+++ testgit/hungry.py
@@ -1,2 +1,8 @@
-print("I'm hungry")
-print("iam not")
+hungry=input("are you hungry?")
+if hungry=="yes":
+    print("eat samosa")
+    print("eat green salad")
+else:
+    print("do your homework")
+    print("play cricket")
+
